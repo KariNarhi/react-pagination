d3d96350a7cbff32c304a03b87bd212e084d56d9
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-02-12 00:17:30 +0200

    Added axios call and rendering logic for posts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Posts from "./components/Posts";
+import axios from "axios";
 import "./App.css";
 
 const App = () => {
+  // Initialize states
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    // Get posts from jsonplaceholder Fake Online REST API
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  const indexOfLastPost = currentPage * postsPerPage; // Get last post index
+  const indexOfFirstPost = indexOfLastPost - postsPerPage; // Get first post index
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost); // Get current posts
+
   return (
-    <div className="container">
-      <h1>My App</h1>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My App</h1>
+      <Posts posts={currentPosts} loading={loading} />
     </div>
   );
 };
